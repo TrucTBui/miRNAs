21f975bab4070ed6b96cf36fc94af77a72f378b8
Thanh Truc Bui <buit@galois.bio.ifi.lmu.de>
2024-11-08 17:16:46 +0100

    extracted refgenome

--- bam_fa2seq.py
+++ bam_fa2seq.py
@@ -25,7 +25,10 @@ def extract_reads_from_sam(sam_file):
     return reads
 
 
-def trim_reads_and_find_variants(all_reads, chromosome, start_range, end_range):
+def trim_reads_and_find_variants(all_reads, location):
+    chromosome, pos_range = location.split(":")
+    start_range, end_range = map(int, pos_range.split("-"))
+
     variant_dict = {}
 
     for read in all_reads:
@@ -67,47 +70,52 @@ def trim_reads_and_find_variants(all_reads, chromosome, start_range, end_range):
         else:
             seq += bases[0]
 
+    #TODO: compare
+
+
     return seq, variants
 
-def extract_SNP_from_vcf(vcf,chrom, start, end, outfile):
-    # Search for SNPs in a specific rrgion
-    cmd = f"zcat {vcf} | grep  -E '^{chrom}|^chr{chrom}' | awk -F'\t' '$2 > {start} && $2 < {end}'>> {outfile}"
-    subprocess.run(cmd, shell=True, check=True)
+def extract_ref_genome(fa, locations):
+    cmd = f"samtools faidx {fa} -r {locations}"
+    result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, text=True)
 
-    snp = {}
+    seqs= {}
+    seq =""
+    location = ""
 
-    with open(outfile,"r") as f:
-        for line in f:
-            if line.startswith("#") or not (line.startswith(f"{chrom}") or line.startswith(f"chr{chrom}")):
-                continue
-            fields = line.split()
-            pos = fields[1]  # Position of the SNP
-            ref = fields[3]  # Base in ref genome
-            alt = fields[4]  # Base in the genome being studied
-            snp[pos] = f"{ref}/{alt}"
+    for line in result.stdout.splitlines():
+        if line.startswith(">"):  # Skip header lines
+            seqs[location] = seq
+            location = line[1:]
+            seq =""
+        else:
+            seq += line.strip()
 
-    return snp
+    return seqs
 
-def parse_bam_and_vcf(path):
+
+def parse_genome(path):
     genomes = []
     with open(path, 'r') as f:
         for line in f:
             if line.startswith("#"):
                 continue
-            genomes.append(line.strip().split())
+            genomes.append(line.strip())
     return genomes
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-g", "--genome", type=str, required=True, help="file containing path to genome in bam format and vcf file with SNP information")
-parser.add_argument("-l", "--location", type=str, nargs="+", required=True, help="genome location to extract reads, e.g. 9:1000-1005 for positions 1000-1005 of chromosome 9")
+parser.add_argument("-g", "--genome", type=str, required=True, help="file containing path to genome(s) in bam format")
+parser.add_argument("-r", "--reference", type=str, required=True, help="reference genome in fasta format")
+parser.add_argument("-l", "--location", type=str, nargs="+", required=True, help="genome location to extract reads")
 parser.add_argument("-o", "--output", type=str, required=False, help="output file for the result")
 
 args = parser.parse_args()
 genome_path = args.genome
+ref_path = args.reference
 output = args.output
 
 # Parse the input path to a list of bam files (index 0) and vcf files(index1)
-genomes= parse_bam_and_vcf(genome_path)
+genomes= parse_genome(genome_path)
 
 locations=[]
 
@@ -118,47 +126,42 @@ if os.path.isfile(args.location[0]):  # Input is a file
 else:  # Input is a list of ranges
     locations = args.location
 
-results = [f"#Genome\tChromosome\tStart\tEnd\tSequence\tSNP\tSNP_Freq\tVariance\tVariance_Freq"]
+ref_genome = extract_ref_genome(ref_path)
+
+results = [f"#Genome\tChromosome\tStart\tEnd\tSequence\tReference\tSNP\tSNP_Freq\tVariance\tVariance_Freq"]
 
 for genome in genomes:
-    bam = genome[0]
-    vcf = genome[1]
 
-    genome_basename = os.path.splitext(os.path.basename(bam))[0]
+    genome_basename = os.path.splitext(os.path.basename(genome))[0]
 
     genome_start_time = time.perf_counter()  # runtime measurement
 
     # Extract all reads for the specified locations
     combined_locations = ' '.join(locations)
     temp_sam_path = f"/tmp/{genome_basename}.sam"
-    command1 = f"(cd /mnt/proj/software && samtools view {bam} {combined_locations}) > {temp_sam_path}"
+    command1 = f"(cd /mnt/proj/software && samtools view {genome} {combined_locations}) > {temp_sam_path}"
     subprocess.run(command1, shell=True, check=True)
 
     all_reads = extract_reads_from_sam(temp_sam_path)
 
     for location in locations:
-        # Parse chromosome and range
+        # Extract ref genome at the current range
+        ref = ref_genome.get(location)
+
+        # Process reads and find variants for the current location
+        seq, variants = trim_reads_and_find_variants(all_reads, location)
+
         chrom, pos_range = location.split(":")
         start_pos, end_pos = map(int, pos_range.split("-"))
 
-        # Process reads and find variants for the current location
-        seq, variants = trim_reads_and_find_variants(all_reads, chrom, start_pos, end_pos)
-
-        # Find SNPs comparing with reference genome in the current range
-        tmp_vcf_extracts = f"/tmp/{genome_basename}.vcf"
-        snps = extract_SNP_from_vcf(vcf,chrom,start_pos,end_pos,tmp_vcf_extracts)
-
-        # Clean up
-        os.remove(f"/tmp/{genome_basename}.vcf")
-
         if output:
-            results.append(f"{genome_basename}\t{chrom}\t{start_pos}\t{end_pos}\t{seq}\t"
-                           f"{';'.join(f'{pos}:{snp}' for pos, snp in snps.items())}\t"
-                           f"{len(snps)}\t"
+            results.append(f"{genome_basename}\t{chrom}\t{start_pos}\t{end_pos}\t{seq}\t{ref}"
+                           #f"{';'.join(f'{pos}:{snp}' for pos, snp in snps.items())}\t"
+                           #f"{len(snps)}\t"
                            f"{';'.join(f'{pos}:{counts}' for pos, counts in variants.items())}\t"
                            f"{len(variants)}\t")
         else:
-            print(f"Genome: {bam}\nLocation: {location}")
+            print(f"Genome: {genome}\nLocation: {location}")
             print(f"Sequence: {seq}")
             print("Differences and frequencies:")
             for pos, counts in variants.items():
